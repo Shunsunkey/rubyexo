2b43ad1901fd9fba34e2cffb3d3ff36136e0be30
root <root@DESKTOP-537TTPD>
2024-04-02 11:13:44 +0200

    modification exo 15

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,8 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 row = gets.chomp.to_i
+if row < 1 || height > 25
+    puts "Le nombre d'étages doit être compris entre 1 et 25."
 puts "Voici la pyramide :"
 for row in 0..row
     row.times {print "#"}
