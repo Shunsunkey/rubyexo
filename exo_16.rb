347a618b5889cc4caed38a6573ca00b988960213
Shunsunkey <45863936+Shunsunkey@users.noreply.github.com>
2024-04-02 11:33:18 +0200

    Delete exo_16.rb
    modification nom

--- exo_16.rb
+++
@@ -1,9 +0,0 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-print "> "
-rows = gets.chomp.to_i
-puts "Voici la pyramide :"
-(rows + 1).times do |row|
-    (rows - row).times { print " " }
-    row.times { print "#" }
-    puts
-  end
